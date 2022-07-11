415e79194357067a0724663074dc8e1f5108be39
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-11 20:22:11 +0530

    Add files via upload

---
+++ if_statement_Part2.py
@@ -0,0 +1,46 @@
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a<b):
+#     print(f"{a}<{b} is True")
+# if(a>b):
+#     print(f"{a}<{b} is False")
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a<b):
+#     print(f"{a}<{b} is True")
+#     print("Hey Python")
+#     print("Hello World")
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a>b):
+#     print(f"{a}>{b} is True")
+#     print("hello world")
+# print("Try Next Time")
+
+# a=int(input('Enter the value of a:'))
+# b=int(input('Enter the value of b:'))
+# if(a<b):
+#     print(f"{a}<{b} is True")
+# if(a>b):
+#     print(f"{a}>{b} is True")
+# if(a==b):
+#     print("f{a}=={b} is True")
+# if(a<=b):
+#     print(f"{a}<={b} is True")
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a<b):
+#     print(f"{a} is less than {b}")
+# else:
+#     print(f"{a} is greater than {b}")
+
+# n=int(input("Enter any Integer Vaalue:"))
+# if(n%2==0):
+#     print(f"{n} is an Even number")
+# else:
+#     print(f"{n} is an odd number")
+
+
