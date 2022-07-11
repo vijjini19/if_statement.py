327d768faea4c4551c58bec204fcb08b1d4a5503
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-11 20:21:17 +0530

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,45 @@
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# a,b=[int(x) for x in input().split(' ')]
+# print("Enter the value of",(a+b));
+
+# a=int(input("Enter the value of a:"))
+# if(a==5):
+#     print("Yes Equal")
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a>b):
+#     print("yes")
+
+# a,b=[int(x) for x in input("Enter the value of a and b").split(' ')]
+# if(a>b):
+#     print(f"{a}>{b} is {a>b}")
+# else:
+#     print(f"{a}>{b} is {a>b}")
+
+# a,b=[int(x) for x in input("Enter the value of a and b").split(' ')]
+# if(a>b):
+#     print("True")
+# else:
+#     print("False")
+
+# a,b=[int(x) for x in input("Enter the value of a and b").split(' ')]
+# if(a>b): print("a is greater then b")
+# if(a<b): print("a is less than b")
+# if(a==b): print('a is equal to b')
+# if(a>=b): print("a is greater than or equal to b")
+# if(a<=b): print("a is less than or equal to b")
+# if(a!=b): print("a is not equal to b")
+
+# a,b=[int(x) for x in input("Enter the value of a and b").split(' ')]
+# if(a>b): print("a is greater then b")
+# elif(a<b): print("a is less than b")
+# elif(a==b): print('a is equal to b')
+# elif(a>=b): print("a is greater than or equal to b")
+# elif(a<=b): print("a is less than or equal to b")
+# elif(a!=b): print("a is not equal to b")
+
+
+
+
